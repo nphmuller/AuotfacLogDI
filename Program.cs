40d8aa7a95f1b633e685a61182ef0b5a0f30dac0
Nick Muller <muller_nicky@hotmail.com>
2017-10-23 10:46:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Autofac.Extensions.DependencyInjection;
+
+namespace AutofacLogDI
+{
+    public class Program
+    {
+        public static void Main() => BuildWebHost().Run();
+
+        public static IWebHost BuildWebHost() =>
+            WebHost.CreateDefaultBuilder()
+                .ConfigureServices(s => s.AddAutofac())
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Features.ResolveAnything;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AutofacLogDI
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutofac();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterSource(
+                new AnyConcreteTypeNotAlreadyRegisteredSource()
+                    .WithRegistrationsAs(b => b.InstancePerLifetimeScope()));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        {
+        }
+    }
+}
